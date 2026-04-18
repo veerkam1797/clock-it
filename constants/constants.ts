aad24023273af314cd166a4f46c5964c6011ab54
veerkam1797 <your-email@domain.com>
2026-04-18 22:51:31 +0530

    feat: add onboarding screen assets, constants, and expo-linear-gradient dependency

---
+++ constants/constants.ts
@@ -0,0 +1,30 @@
+import { Drawables } from "./drawable";
+
+export const Constants = {
+  OnboardingScreens: [
+    {
+      id: 1,
+      title: 'Welcome to Clock It',
+      description: 'Make Smarter Decisions! Set clear timelines for projects and celebrate achievements.',
+      image: Drawables.onbaording[1],
+    },
+    {
+      id: 2,
+      title: 'Manage Stress Effectively',
+      description: 'Stay Balanced! Track your workload and maintain a healthy stress level with ease.',
+      image: Drawables.onbaording[2],
+    },
+    {
+      id: 3,
+      title: 'Plan for Success',
+      description: 'Your Journey Starts Here! Earn achivement badges as you conquer your tasks. Let\'s get started!',
+      image: Drawables.onbaording[3],
+    },
+    {
+      id: 4,
+      title: 'Navigate Your Work Journey Efficently & Easily',
+      description: 'Increase your work management & career development radically.',
+      image: Drawables.onbaording[4],
+    }
+  ],
+}
---
+++ constants/drawable.ts
@@ -0,0 +1,8 @@
+export const Drawables = {
+  onbaording: {
+    1: require('@/assets/images/onboarding/onboarding-1.png'),
+    2: require('@/assets/images/onboarding/onboarding-2.png'),
+    3: require('@/assets/images/onboarding/onboarding-3.png'),
+    4: require('@/assets/images/onboarding/onboarding-4.png'),
+  }
+}
